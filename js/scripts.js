9b08b0812b65f93daec45e8cc6be96f2f9eb85e1
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-11-10 06:43:51 -0600

    Add scirpts to /js/script.js and styles to /css/styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 /*-----Javascript for js-create-design-website */
 $(document).ready(function () {
-	
+   console.log('document is ready in jQuery!')
 
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	console.log('document is ready in plain JavaScript!');
+   
 });

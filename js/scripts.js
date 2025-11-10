0fb3be8dd003d8fbc2b98a27afc3ce9eaf1c601d
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-11-10 09:27:18 -0600

    Add animation to measurment inputs and improve styles for radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
 /*-----Javascript for js-create-design-website */
+$(window).on('load', function () {
+	// makes sure that whole site is loaded
+	console.log('and the Window has loaded,');
+	$('.effect-07').val('');
+
+	$('.effect-07').focusout(function () {
+		if ($(this).val() !== '') {
+			$(this).addClass('has-content');
+		} else {
+			$(this).removeClass('has-content');
+		}
+	});
+});
+
 $(document).ready(function () {
    console.log('document is ready in jQuery!')
+
 
 });
 
 document.addEventListener('DOMContentLoaded', () => {
 	console.log('document is ready in plain JavaScript!');
-   
+ 
 });
